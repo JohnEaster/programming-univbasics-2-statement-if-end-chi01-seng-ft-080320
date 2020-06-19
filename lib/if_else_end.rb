5605d8dea2bfbf9a6dc60f387265e36fe4e45d52
John Easter <john@owltestprep.com>
2020-06-19 15:33:22 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
-chance_of_rain = 0.2
+chance_of_rain = 1
 puts "Let's go outside!"
 if chance_of_rain > 0.5
   puts "Pack and umbrella!"
 else
-  puts "Enjoy the find day!"
+  puts "Enjoy the fine day!"
 end 
 puts "Oh, and always wear sunscreen!"
